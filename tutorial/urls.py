60a06fec1065c64c0d90f1618928c621bd0789d8
HargovindArora <hargovinds50@gmail.com>
2018-02-06 22:36:32 +0530

    Added User ListAndDetail view and url conf to appURLs

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -19,6 +19,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('snippets.urls')),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
