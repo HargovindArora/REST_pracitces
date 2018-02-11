3749056b67c1d71eb0cb4f59781fc691f44456c0
HargovindArora <hargovinds50@gmail.com>
2018-02-11 21:06:54 +0530

    Small debugging

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -21,10 +21,10 @@ schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
     url(r'^schema/$', schema_view),
-    url(r'^admin/', admin.site.urls),
     url(r'^', include('snippets.urls')),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
 
 urlpatterns += [
-    url(r'^api-auth/', include('rest_framework.urls')),
+
 ]
